3340ac4bf93e9fa06a297c0b847b654f8e86cbea
Kashish Gupta <kashishgup100@gmail.com>
2020-12-05 11:53:25 +0530

    api call for textgears

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 
+
 function addFileTextToTextArea(event){
 
     event.preventDefault();
-
-    
-
     var file = document.getElementById("myFile").files[0];
    
     if(!validateFile(file.name)){
         alert("Please upload a valid file");
+        return;
     }
     var reader = new FileReader();
     reader.onload = function (e) {
     var textArea = document.getElementById("myTextArea");
     textArea.value = e.target.result;
+    spellCheck(e.target.result);
     };
     reader.readAsText(file);
+   
+    
 }
 
 function validateFile(filename) {
@@ -24,4 +26,19 @@ function validateFile(filename) {
         return false;
     }
     return true;
+  }
+
+  function spellCheck(text){
+
+    let base_url = "https://api.textgears.com/grammar?key=1gVny1rfj02gy7kY&text="+text;
+    let fetchData = {
+        method: 'GET',
+        headers: new Headers()
+    }
+
+    fetch(base_url,fetchData)
+    .then(response => response.json())
+    .then(json => console.log(json))    
+    .catch(err => console.log('Request Failed', err)); 
+
   }
